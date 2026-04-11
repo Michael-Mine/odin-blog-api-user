1e5a01cce6dab3f9d6f1e49eede863f68b1a1e8b
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-11 16:32:21 +0100

    feat: add Home comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 const posts = [
   {
     id: 1,
@@ -17,32 +15,31 @@ const posts = [
   },
 ];
 
-const post = {
-  id: 1,
-  title: "Thinking in React",
-  content:
-    "React can change how you think about the designs you look at and the apps you build. When you build a user interface with React, you will first break it apart into pieces called components. Then, you will describe the different visual states for each of your components. Finally, you will connect your components together so that the data flows through them. In this tutorial, we’ll guide you through the thought process of building a searchable product data table with React.",
-  datePublished: "2026-03-30T00:01:01.000Z",
-};
-
 function App() {
   return (
     <>
       {/* Navbar */}
-      {/* home or post */}
       <h1>Mr Mine Blog API</h1>
-      <PostCardItem />
+      {/* home or post */}
+      <Home />
+      {/* Footer */}
     </>
   );
 }
 
 // compenent static, state, proptype, css, tests/snapshot
 
-// Home
-// - Post Card
-// -- Post Card Item
+function Home() {
+  return (
+    <div>
+      {posts.map((post) => {
+        return <PostItem key={post.id} post={post} />;
+      })}
+    </div>
+  );
+}
 
-function PostCardItem() {
+function PostItem({ post }) {
   const date = dateDisplay(post.datePublished);
 
   return (
