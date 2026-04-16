4cb1a3acd6880ed661d36ab5ed00f3cea81cfdda
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-16 14:38:20 +0100

    style: add Post module css

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,13 @@
 import { useParams } from "react-router";
 import usePost from "../hooks/usePost";
 import { useEffect } from "react";
+import styles from "../styles/Post.module.css";
+
+function dateDisplay(datePublished) {
+  const timestamp = Date.parse(datePublished);
+  const date = new Date(timestamp);
+  return date.toDateString();
+}
 
 function Post() {
   const { setPostId, post, error, loading } = usePost();
@@ -13,11 +20,21 @@ function Post() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
+  const date = dateDisplay(post.datePublished);
+
   return (
-    <>
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-    </>
+    <div>
+      <img
+        src={post.picUrl ? post.picUrl : "https://picsum.photos/400/600"}
+        alt="blog post picture"
+        className={styles.image}
+      />
+      <div className={styles.post}>
+        <h2>{post.title}</h2>
+        <p>By Mr Mine - Published {date}</p>
+        <p>{post.content}</p>
+      </div>
+    </div>
   );
 }
 
