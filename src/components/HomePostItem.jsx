6b7c705a2f9971287b26a67ab59c91371c361667
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-21 08:59:42 +0100

    refactor: reduce code in HomePostItem

--- src/components/HomePostItem.jsx
+++ src/components/HomePostItem.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router";
-import styles from "../styles/HomePostItem.module.css";
 import formatDate from "../utils/formatDate";
+import styles from "../styles/HomePostItem.module.css";
 
 function HomePostItem({ post }) {
-  const url = "/post/" + post.id;
+  const url = `/post/${post.id}`;
   const date = formatDate(post.datePublished);
 
   return (
     <Link to={url} className={styles.item}>
       <img
-        src={post.picUrl ? post.picUrl : "https://picsum.photos/200/300"}
+        src={post.picUrl || "https://picsum.photos/200/300"}
         alt="blog post picture"
         className={styles.image}
       />
