4456af49420ef7a6ff5b2f37490f185a44eee70d
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-24 21:45:09 +0100

    test: add mocked child components for Home page

--- src/components/Comments.test.jsx
+++ src/components/Comments.test.jsx
@@ -3,11 +3,13 @@ import { render, screen } from "@testing-library/react";
 import { RouterProvider, useParams } from "react-router";
 import useComments from "../hooks/useComments";
 import Comments from "./Comments";
+import CommentItem from "./CommentItem";
 
 vi.mock("react-router");
 vi.mock("../hooks/useComments");
+vi.mock("./CommentItem");
 
-describe("Testing Home page", () => {
+describe("Testing Comments page", () => {
   it("renders loading text", () => {
     vi.mocked(useParams).mockReturnValue({ postId: "1" });
     vi.mocked(useComments).mockReturnValue({
@@ -49,5 +51,22 @@ describe("Testing Home page", () => {
     expect(
       screen.getByRole("heading", { name: "0 Comments" }),
     ).toBeInTheDocument();
+  });
+
+  it("renders correct number in comments heading", () => {
+    vi.mocked(useParams).mockReturnValue({ postId: "1" });
+    vi.mocked(useComments).mockReturnValue({
+      comments: [1, 2, 3, 4],
+      error: null,
+      loading: false,
+    });
+    vi.mocked(CommentItem).mockReturnValue(<p>Mock Comment</p>);
+
+    render(<Comments />);
+
+    expect(
+      screen.getByRole("heading", { name: "4 Comments" }),
+    ).toBeInTheDocument();
+    expect(screen.getAllByText("Mock Comment")).toHaveLength(4);
   });
 });
--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -1,22 +1,57 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { createMemoryRouter, RouterProvider } from "react-router";
+import {
+  createMemoryRouter,
+  RouterProvider,
+  useOutletContext,
+} from "react-router";
 import routes from "../router/routes";
 import Home from "./Home";
+import HomePostItem from "../components/HomePostItem";
+
+vi.mock("react-router");
+vi.mock("../components/HomePostItem");
 
 describe("Testing Home page", () => {
-  it("renders loading text", () => {
-    const router = createMemoryRouter(routes, { initialEntries: ["/"] });
-    const allPosts = [];
-    const error = null;
-    const loading = true;
+  // it("renders loading text", () => {
+  //   const router = createMemoryRouter(routes, { initialEntries: ["/"] });
+  //   const allPosts = [];
+  //   const error = null;
+  //   const loading = true;
 
-    render(
-      <RouterProvider router={router}>
-        <Home context={[allPosts, error, loading]} />
-      </RouterProvider>,
-    );
+  //   render(
+  //     <RouterProvider router={router}>
+  //       <Home context={[allPosts, error, loading]} />
+  //     </RouterProvider>,
+  //   );
+
+  //   expect(screen.getByText("Loading...")).toBeInTheDocument();
+  // });
+
+  it("renders loading text", () => {
+    vi.mocked(useOutletContext).mockReturnValue([[], null, true]);
+
+    render(<Home />);
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("renders error text", () => {
+    vi.mocked(useOutletContext).mockReturnValue([[], "error", false]);
+
+    render(<Home />);
+
+    expect(
+      screen.getByText("A network error was encountered"),
+    ).toBeInTheDocument();
+  });
+
+  it("renders correct number of children components", () => {
+    vi.mocked(useOutletContext).mockReturnValue([[1, 2, 3, 4], null, false]);
+    vi.mocked(HomePostItem).mockReturnValue(<p>Mock Post</p>);
+
+    render(<Home />);
+
+    expect(screen.getAllByText("Mock Post")).toHaveLength(4);
   });
 });
