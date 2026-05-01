9abc3529aa2f6acecbb31fc20aab9fb3ea3893af
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-01 17:19:29 +0100

    test: update for HomePostItem

--- src/components/HomePostItem.test.jsx
+++ src/components/HomePostItem.test.jsx
@@ -20,14 +20,14 @@ describe("Testing HomePostItem Component", () => {
     );
 
     const link = screen.getByRole("link", {
-      name: "blog post picture Mon Mar 30 2026 Test Title Test content text",
+      name: "blog post picture Mon Mar 30 2026 Test Title Test content text...",
     });
     const date = screen.getByRole("heading", {
       name: "Mon Mar 30 2026",
     });
     const title = screen.getByRole("heading", { name: "Test Title" });
     const image = screen.getByAltText("blog post picture");
-    const content = screen.getByText("Test content text");
+    const content = screen.getByText("Test content text...");
 
     expect(link).toBeInTheDocument();
     expect(date).toBeInTheDocument();
